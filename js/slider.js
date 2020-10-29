91112bfe7baef210f2f55b2ea0709ac1e876fc1b
Vladislav <masonwhite@yandex.ru>
2020-10-29 18:01:35 +0300

    add autoplay slider

--- js/slider.js
+++ js/slider.js
@@ -2,6 +2,7 @@ $('.slider').slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
     prevArrow: '<button type="button" class="slider-arrow slider-arrow--left"></button>',
     nextArrow: '<button type="button" class="slider-arrow slider-arrow--right"></button>'
 });
